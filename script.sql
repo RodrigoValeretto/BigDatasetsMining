d1166d80d49b155f36d85c151fcec65fcf4c11c5
rodrigovaleretto <rodrigoaugustovaleretto@gmail.com>
2021-12-01 18:02:29 -0300

    Implementação até exc 6 - iniciado

--- script.sql
+++ script.sql
@@ -2,19 +2,13 @@
 /*
 	Trabalho SCC0244 - Mineracao de grandes bases de dados 
 	Alunos:
-	André Bermudes Viana			NUSP: 10684580
+	Andre Bermudes Viana			NUSP: 10684580
 	Joao Pedro Fidelis Belluzzo		NUSP: 10716661
 	Leonardo Cerce Guioto			NUSP: 10716640
 	Rodrigo Augusto Valeretto		NUSP: 10684792
 */
 ------------------------------------------------------------
-
-
--- Criando Database e Schema
-create database "fapesp-covid19";
-create schema "hsl";
-
-------------------- EXERCÍCIO 01 -------------------
+------------------- EXERCICIO 01 -------------------
 
 -- Criando tabelas principais
 drop table if exists Pacientes cascade;
@@ -58,7 +52,8 @@ create table Desfechos(
 );
 
 -- Configura banco para aceitar data no estilo DD/MM/YYYY
-SET datestyle = 'ISO,DMY';
+set datestyle = 'ISO,DMY';
+
 
 -- Preenchendo tabelas com valores dos csvs
 copy Pacientes from '/HSL_Junho2021/HSL_Pacientes_4.csv' delimiter '|' csv header;
@@ -74,8 +69,11 @@ update pacientes set aa_nascimento = '1930' where aa_nascimento = 'AAAA';
 update pacientes set aa_nascimento = null where aa_nascimento = 'YYYY';
 alter table pacientes alter column aa_nascimento type integer using aa_nascimento::integer;
 
+create extension unaccent;
+update exames set de_analito = lower(unaccent(de_analito));
 
-------------------- EXERCÍCIO 02 -------------------
+
+------------------- EXERCICIO 02 -------------------
 
 -- 2.1 - TODO
 
@@ -98,7 +96,7 @@ select
 	date_part('year', now()) - percentile_cont(0.75) within group (order by aa_nascimento asc) as "3o_quart"
 from pacientes group by ic_sexo;
 
--- Conta quantos homens e mulheres nasceram em cada decada (O valor NULL corresponde à pessoas que nao tem dados)
+-- Conta quantos homens e mulheres nasceram em cada decada (O valor NULL corresponde a pessoas que nao tem dados)
 select ic_sexo, floor(aa_nascimento/10)*10 as decada, count(id_paciente) as n_pacientes
 from pacientes
 group by decada, ic_sexo order by ic_sexo, decada asc;
@@ -126,16 +124,16 @@ select avg(n_exames) as media_exames_F from (
 	) as count_exames;
 	
 -- Conta quantos exames de covid foram solicitados
-select count(id_exame) as qnt_exames_covid from exames e where de_analito = 'Coronavírus (2019-nCoV)';
+select count(id_exame) as qnt_exames_covid from exames e where de_analito = 'coronavirus (2019-ncov)';
 
 -- Conta quantos exames de covid possuem resultado positivo
 select count(id_exame) as qnt_exames_covid_positivo from exames e
-where de_analito = 'Coronavírus (2019-nCoV)' and de_resultado like 'DETECTADO%';
+where de_analito = 'coronavirus (2019-ncov)' and upper(de_resultado) like 'DETECTADO%';
 
--- Questão estranha, perguntar pro prof
+-- Questao estranha, perguntar pro prof
 select p.aa_nascimento, count(e.de_resultado) from exames e
 join pacientes p on p.id_paciente = e.id_paciente 
-where de_analito = 'Coronavírus (2019-nCoV)'
+where de_analito = 'coronavirus (2019-ncov)'
 group by p.aa_nascimento
 order by p.aa_nascimento asc;
 
@@ -149,26 +147,26 @@ select p.ic_sexo, mode() within group (order by de_desfecho) from desfechos d
 join pacientes p on d.id_paciente = p.id_paciente
 
 
-------------------- EXERCÍCIO 03 -------------------
+------------------- EXERCICIO 03 -------------------
 -- Calcular numero de mortes de pessoas que tiveram resultado positivo em Covid-19
--- e também numero de mortes agrupados por decada de nascimento dos pacientes.
+-- e tambï¿½m numero de mortes agrupados por decada de nascimento dos pacientes.
 select count(*) as n_mortes from (
     select id_paciente as id_pacientes_positivos from exames e
-    where de_analito = 'Coronavírus (2019-nCoV)' and de_resultado like 'DETECTADO%'   
+    where de_analito = 'coronavirus (2019-ncov)' and de_resultado like 'DETECTADO%'   
 ) as consulta_positivos join desfechos d on d.id_paciente = id_pacientes_positivos
 where d.dt_desfecho like 'DDMMAA'
 
 select floor(p.aa_nascimento/10)*10 as decada_de_nascimento, count(p.id_paciente) as n_mortes
 from (
     select id_paciente as id_pacientes_positivos from exames e
-    where de_analito = 'Coronavírus (2019-nCoV)' and de_resultado like 'DETECTADO%'   
+    where de_analito = 'coronavirus (2019-ncov)' and de_resultado like 'DETECTADO%'   
 ) as consulta_positivos join desfechos d on d.id_paciente = id_pacientes_positivos
 join pacientes p on p.id_paciente = id_pacientes_positivos 
 where d.dt_desfecho like 'DDMMAA'
 group by decada_de_nascimento order by decada_de_nascimento asc;
 
 
-------------------- EXERCÍCIO 04 -------------------
+------------------- EXERCICIO 04 -------------------
 -- 4.A - Select para buscar mais novos e mais velhos de cada cidade usando apenas group by
 select p.cd_municipio, p.id_paciente, date_part('year', now()) - p.aa_nascimento as idade from (
     select cd_municipio as min_max_mun, max(aa_nascimento) as ano_max, min(aa_nascimento) as ano_min
@@ -189,8 +187,56 @@ order by cd_municipio, idade asc
 
 -- 4.C - Select para buscar mais novos e mais velhos de cada cidade usando window function
 
-select cd_municipio, id_paciente, aa_nascimento,
-    min(aa_nascimento) over(partition by cd_municipio) ano_min,
-    max(aa_nascimento) over(partition by cd_municipio) ano_max
-from pacientes p 
-order by cd_municipio, aa_nascimento
+select cd_municipio, id_paciente, date_part('year', now()) - aa_nascimento as idade
+from(
+  select cd_municipio, id_paciente, aa_nascimento,
+      min(aa_nascimento) over(partition by cd_municipio) ano_min,
+      max(aa_nascimento) over(partition by cd_municipio) ano_max
+  from pacientes p) as p
+where p.aa_nascimento = p.ano_max or p.aa_nascimento = p.ano_min
+order by cd_municipio, idade;
+
+
+------------------- EXERCICIO 05 -------------------
+-- 5.A - Consulta para mostrar quaais analitos podem ser medidos em exames de 'hemograma' em cada hospital
+-- Versao sem window function
+select de_origem, array_agg(distinct de_analito) from exames e
+where upper(e.de_exame) like '%HEMOGRAMA%'
+group by de_origem;
+    
+-- Versao com window function
+select de_origem, array_agg from ( 
+select
+    de_origem,
+    array_agg(de_analito) over (partition by de_origem),
+    row_number() over (partition by de_origem)
+from
+    exames e
+where
+    upper(e.de_exame) like '%HEMOGRAMA%' 
+    ) as p 
+where row_number = 1
+
+-- 5.B
+
+------------------- EXERCICIO 06 -------------------
+drop view if exists exames_colesterol;
+
+-- Cria tabela de auxilio
+create view exames_colesterol as
+select distinct
+    de_analito,
+    id_paciente,
+    id_atendimento,
+    dt_coleta,
+    cd_unidade,
+    avg(to_number(de_resultado, '99999999D9999999')) over (partition by id_paciente, dt_coleta, de_analito)
+from exames e
+where lower(e.de_exame) like '%colesterol%' and de_resultado not like '%impossibilita%'
+order by id_paciente, id_atendimento, id_paciente
+
+
+
+
+
+
