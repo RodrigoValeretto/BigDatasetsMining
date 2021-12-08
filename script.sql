b983fea23a2c98eb35103c6d51dbe841f8df00a7
André Viana <andrebermudesviana@gmail.com>
2021-12-08 17:21:10 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -54,7 +54,6 @@ create table Desfechos(
 -- Configura banco para aceitar data no estilo DD/MM/YYYY
 set datestyle = 'ISO,DMY';
 
-
 -- Preenchendo tabelas com valores dos csvs
 copy Pacientes from '/HSL_Junho2021/HSL_Pacientes_4.csv' delimiter '|' csv header;
 
@@ -69,6 +68,15 @@ copy Desfechos from '/HSL_Junho2021/HSL_Desfechos_4.csv' delimiter '|' csv heade
 update pacientes set aa_nascimento = '1930' where aa_nascimento = 'AAAA';
 update pacientes set aa_nascimento = null where aa_nascimento = 'YYYY';
 alter table pacientes alter column aa_nascimento type integer using aa_nascimento::integer;
+
+-- Trocando o valor do UF para nulo nos dados anonimizados
+update pacientes set cd_uf = null where cd_uf = 'UU';
+
+-- Trocando o valor do Município para nulo nos dados anonimizados ou de estrangeiros
+update pacientes set cd_municipio = null where cd_municipio = 'MMMM';
+
+-- Trocando o valor do CEP para nulo nos dados anonimizados ou de estrangeiros
+update pacientes set cd_cepreduzido = null where cd_cepreduzido = 'CCCC';
 
 -- Removendo todos os acentos dos analitos na tabela de exames
 create extension if not exists unaccent;
@@ -121,7 +129,153 @@ and unaccent(de_resultado) ilike 'indeterminado';
 
 ----------------------- EXERCICIO 02 -----------------------
 
--- 2.1 - TODO
+-- 2.1
+-------------------- TABELA Pacientes
+-------- ID_PACIENTE
+-- Número de Pacientes
+select count(id_paciente) as n_atendimentos from pacientes p;
+
+-------- IC_SEXO
+-- Distribuição dos sexos
+select ic_sexo, count(ic_sexo) from pacientes p
+group by ic_sexo;
+
+-------- AA_NASCIMENTO
+-- Número de Idades Anonimizadas (nulas após nossa limpeza)
+select count(*) as n_nulos_idade from pacientes p
+where aa_nascimento is null;
+-- Variância da idade
+select var_samp(aa_nascimento) from pacientes p;
+-- Desvio Padrão da idade
+select stddev_samp(aa_nascimento) from pacientes p;
+
+-------- CD_PAIS
+-- Número de Estrangeiros
+select count(*) as n_estrangeiros from pacientes p
+where cd_pais = 'XX';
+
+-------- CD_UF
+-- Distribuição dos UF
+select cd_uf, count(cd_uf) from pacientes p
+where cd_uf is not null
+group by cd_uf;
+-- Número de Anonimizados (nulos após nossa limpeza)
+select count(*) as n_nulos from pacientes p
+where cd_uf is null;
+
+-------- CD_MUNICIPIO
+-- Distribuição dos Municipios
+select cd_municipio, count(cd_municipio) from pacientes p
+where cd_municipio is not null
+group by cd_municipio;
+-- Número de Anonimizados (nulos após nossa limpeza)
+select count(*) as n_nulos from pacientes p
+where cd_municipio is null;
+
+-------- CD_CEPREDUZIDO
+-- Distribuição dos CEPS
+select cd_cepreduzido, count(cd_cepreduzido) from pacientes p
+where cd_cepreduzido is not null
+group by cd_cepreduzido;
+-- Número de Anonimizados (nulos após nossa limpeza)
+select count(*) as n_nulos from pacientes p
+where cd_cepreduzido is null;
+
+-------------------- TABELA Exames
+-------- ID_PACIENTE
+-- Número de pacientes que pediram exames
+select count(distinct id_paciente) as n_atendimentos from exames e;
+
+-------- ID_ATENDIMENTO
+-- Número de atendimentos
+select count(distinct id_atendimento) as n_atendimentos from exames e;
+-- Média de exames por atendimento	
+select
+	avg(numero_exames)
+from
+	(
+	select
+		count(*) over (partition by id_atendimento) numero_exames
+	from
+		exames e) as a;
+
+-------- DT_COLETA
+-- Média de coletas por dia	
+select
+	avg(numero_coletas) as media_coletas_dia
+from
+	(
+	select
+		count(*) over (partition by dt_coleta) numero_coletas
+	from
+		exames e) as a;
+	
+-------- DE_ORIGEM
+-- Número de origens existentes
+select count(distinct de_origem) as n_origens from exames e;
+-- Distribuição das origens
+select de_origem, count(de_origem) from exames e
+where de_origem is not null
+group by de_origem;
+
+-------- DE_EXAME
+-- Número total de exames realizados
+select count(de_exame) as numero_exames from exames e;
+-- Número de tipos de exames existentes na tabela
+select count(distinct de_exame) as numero_distinto_exames from exames e;
+
+-------- DE_ANALITO
+-- Número de tipos de analitoss existentes na tabela
+select count(distinct de_analito) as numero_distinto_analitos from exames e;
+
+-------- DE_RESULTADO
+-- ?????
+-------- CD_UNIDADE
+-- ?????
+-------- DE_VALOR_REFERENCIA
+-- ?????
+
+-------------------- TABELA Desfechos
+-------- DT_ATENDIMENTO
+-- Media de atendimentos por dia	
+select
+	avg(numero_atendimentos) as media_atendimentos
+from
+	(
+	select
+		count(*) over (partition by dt_atendimento) numero_atendimentos
+	from
+		desfechos d) as a;
+
+-------- DE_TIPO_ATENDIMENTO
+-- Tipos distintos de atendimentos
+select distinct de_tipo_atendimento tipos_unicos from desfechos d;
+-- Distribuicao dos tipos de atendimento
+select de_tipo_atendimento, count(de_tipo_atendimento) from desfechos d
+where de_tipo_atendimento is not null
+group by de_tipo_atendimento;
+
+-------- ID_CLINICA
+-- Numero de clinicas
+select  count(distinct id_clinica) n_clinicas from desfechos d;
+
+-------- DE_CLINICA
+-- ?????
+
+-------- DT_DESFECHO
+-- Media de desfechos por dia	
+select
+	avg(numero_desfechos) as media_atendimentos
+from
+	(
+	select
+		count(*) over (partition by dt_desfecho) numero_desfechos
+	from
+		desfechos d) as a;
+
+-------- DE_DESFECHO
+-- Tipos distintos de desfechos
+select distinct de_desfecho desfechos_unicos from desfechos d;
 
 -- 2.2
 -- Conta pacientes da base
@@ -186,26 +340,26 @@ order by p.aa_nascimento asc;
 -- 2.4
 select mode() within group (order by de_desfecho) from desfechos;
 
-select d.de_desfecho, d.id_paciente from desfechos d where lower(d.de_desfecho) <> 'alta administrativa'
+select d.de_desfecho, d.id_paciente from desfechos d where lower(d.de_desfecho) <> 'alta administrativa';
 
 -- TODO Ultimo topico
 select p.ic_sexo, mode() within group (order by de_desfecho) from desfechos d
-join pacientes p on d.id_paciente = p.id_paciente
+join pacientes p on d.id_paciente = p.id_paciente;
 
 
 ----------------------- EXERCICIO 03 -----------------------
 -- Calcular numero de mortes de pessoas que tiveram resultado positivo em Covid-19
 -- e tambem numero de mortes agrupados por decada de nascimento dos pacientes.
 select count(*) as n_mortes from (
-    select id_paciente as id_pacientes_positivos from exames e
-    where de_analito = 'coronavirus (2019-ncov)' and de_resultado like 'DETECTADO'   
+    select distinct id_paciente as id_pacientes_positivos from exames e
+    where de_analito = 'DETECCAO COVID' and de_resultado like 'DETECTADO'   
 ) as consulta_positivos join desfechos d on d.id_paciente = id_pacientes_positivos
 where d.dt_desfecho like 'DDMMAA'
 
 select floor(p.aa_nascimento/10)*10 as decada_de_nascimento, count(p.id_paciente) as n_mortes
 from (
-    select id_paciente as id_pacientes_positivos from exames e
-    where de_analito = 'coronavirus (2019-ncov)' and de_resultado like 'DETECTADO'   
+    select distinct id_paciente as id_pacientes_positivos from exames e
+    where de_analito = 'DETECCAO COVID' and de_resultado like 'DETECTADO'   
 ) as consulta_positivos join desfechos d on d.id_paciente = id_pacientes_positivos
 join pacientes p on p.id_paciente = id_pacientes_positivos 
 where d.dt_desfecho like 'DDMMAA'
@@ -484,11 +638,3 @@ values('covid 19, anticorpos iga'),
     "covid 19, anticorpos igm, quimiolumin.-indice" varchar,
     "covid 19, anticorpos totais, eletroquim-indic" varchar,
     "covid 19, anti-spike neutralizantes - indice" varchar)
-
-
-
-
-
-
-
-
